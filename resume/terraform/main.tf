57279b4b66b870580fc6cfb8abcc4cbcb0641356
sumit sunil shelar <87257778+sumit4h9@users.noreply.github.com>
2025-06-24 17:40:31 +0530

    Add files via upload

---
+++ resume/terraform/main.tf
@@ -0,0 +1,37 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "6c378fb3-818c-43a9-8c9e-bfc6ecb08e95"  # 🔁 Replace this with your actual Azure subscription ID
+}
+
+locals {
+  timestamp = timestamp()
+}
+
+resource "azurerm_resource_group" "resume_rg" {
+  name     = "resume-rg"
+  location = "Central India"
+}
+
+resource "azurerm_storage_account" "resume_storage" {
+  name                     = "resumestorage${substr(md5(local.timestamp), 0, 8)}"
+  resource_group_name      = azurerm_resource_group.resume_rg.name
+  location                 = azurerm_resource_group.resume_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  min_tls_version          = "TLS1_2"
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_account_static_website" "resume_static" {
+  storage_account_id = azurerm_storage_account.resume_storage.id
+
+  index_document     = "index.html"
+  error_404_document = "index.html"
+}
+
+output "primary_endpoint" {
+  value = azurerm_storage_account.resume_storage.primary_web_endpoint
+}
+
